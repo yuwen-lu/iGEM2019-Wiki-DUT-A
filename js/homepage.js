3c26200e5e3435ba0ed84e4ce500cb3139c31664
yuwen-lu <wayne.lu1998@gmail.com>
2019-10-04 12:15:42 +0200

    improve homepage design

---
+++ js/homepage.js
@@ -0,0 +1,15 @@
+// load navigation bar and footer
+$(function() {
+  $("#navigation").load("./navigation.html");
+  $("#footer").load("./footer.html");
+});
+
+// Animation on Scroll
+AOS.init({
+  offset: 300,
+  duration: 1000
+});
+AOS.init({
+  offset: 300,
+  duration: 1000
+});
