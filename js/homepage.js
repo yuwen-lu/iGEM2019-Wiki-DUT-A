9185428bdfb08e2675ae3bcdf95ed126e2e53480
yuwen-lu <wayne.lu1998@gmail.com>
2019-10-21 00:42:19 +0800

    add acknowledgement

--- js/homepage.js
+++ js/homepage.js
@@ -11,19 +11,19 @@ AOS.init({
 });
 
 // add the auto scroll behavior to the homepage
-let firstSection = document.getElementsByClassName("section")[0];
-let firstTimeScroll = true;
-let cnt = 0; // used to set a little bit delay of scrolling down, otherwise it would be too fast
+// let firstSection = document.getElementsByClassName("section")[0];
+// let firstTimeScroll = true;
+// let cnt = 0; // used to set a little bit delay of scrolling down, otherwise it would be too fast
 
-window.addEventListener("scroll", () => {
-  let firstSectionTop = firstSection.getBoundingClientRect().top;
-  console.log(firstSectionTop, firstTimeScroll);
-  if (firstSectionTop >= 600 && firstTimeScroll) {
-    // 300 is the offset of AOS init function, not very accurate but works
+// window.addEventListener("scroll", () => {
+//   let firstSectionTop = firstSection.getBoundingClientRect().top;
+//   console.log(firstSectionTop, firstTimeScroll);
+//   if (firstSectionTop >= 600 && firstTimeScroll) {
+//     // 300 is the offset of AOS init function, not very accurate but works
 
-    window.scrollBy(0, firstSectionTop - 80);
-    if (cnt++ > 50) {
-      firstTimeScroll = false;
-    }
-  }
-});
+//     window.scrollBy(0, firstSectionTop - 80);
+//     if (cnt++ > 50) {
+//       firstTimeScroll = false;
+//     }
+//   }
+// });
